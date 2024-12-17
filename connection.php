bd8a79722a0680ffdad4e0559e573a1544322a85
Prajwal08a <prajwalsritej08@gmail.com>
2024-12-17 19:35:07 +0530

    version 1.1

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$database= new mysqli("localhost","root","","SQL_Database_edoc");
+$database= new mysqli("localhost","root","","SQL_Database_eHealthDesk");
 if ($database->connect_error){
     die("Connection failed:  ".$database->connect_error);
 }
