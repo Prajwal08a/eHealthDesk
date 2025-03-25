ea4c68d0d5969c1658ae3f0c0f44180ad06d20bf
A.PRAJWAL SRI TEJ <108971538+Prajwal08a@users.noreply.github.com>
2025-03-25 23:01:36 +0530

    V.3.1

--- SQL_Database_eHealthDesk.sql
+++ SQL_Database_eHealthDesk.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sql_database_edoc`
+-- Database: `sql_database_eHealthDesk`
 --
 
 -- --------------------------------------------------------
